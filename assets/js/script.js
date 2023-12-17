df79dfa6b7c3eb194478a948fc792d2169903094
vitorramon <rvramon16@gmail.com>
2023-12-17 16:41:33 -0400

    feat: add Dark Mode

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+const toogleTheme = document.getElementById("toggleTheme");
+const rootHtml = document.documentElement;
+
+function changeTheme(){
+    const currentTheme = rootHtml.getAttribute("data-theme");
+  
+    currentTheme === "dark" ? rootHtml.setAttribute("data-theme", "light") : rootHtml.setAttribute("data-theme", "dark")
+  
+    toggleTheme.classList.toggle("bi-sun")
+    toggleTheme.classList.toggle("bi-moon-stars")
+}
+
+toggleTheme.addEventListener("click", changeTheme);
